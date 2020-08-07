fe6fff178da54d88830ffaeac9dbcfcacdf7408d
Francis <francisd.billones@gmail.com>
2020-08-07 11:19:58 +0000

    readibility in progress

---
+++ pset1/cash/cash.c
@@ -0,0 +1,43 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    //values
+    float change_float = get_float("How much change do you want?: $");
+    int change_int = round(change_float * 100);
+    int quarters = 0;
+    int dimes = 0; 
+    int nickels = 0; 
+    int pennies = 0;
+    printf("%i\n", change_int);
+    
+    
+    //algorithm
+    while ((change_int - 25) >= 0)
+    {
+        change_int -= 25;
+        quarters += 1;
+    }
+    
+    while ((change_int - 10) >= 0)
+    {
+        change_int -= 10;
+        dimes += 1;
+    }
+    
+    while ((change_int - 5) >= 0)
+    {
+        change_int -= 5;
+        nickels += 1;
+    }
+    
+    while ((change_int - 1) >= 0)
+    {
+        change_int -= 1;
+        pennies += 1;
+    }
+
+    printf("Your change is %i quarters, %i dimes, %i nickels, %i pennies\n", quarters, dimes, nickels, pennies);
+}
---
+++ pset2/readibility.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    int wordCount = 0;
+    int letterCount = 0;
+    int sentenceCount = 0;
+    userText = [];
+    string userIn = get_string("Text: ");
+
+    for (int i = 0; )
+}
