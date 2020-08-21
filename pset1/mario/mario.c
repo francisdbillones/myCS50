9758220781b8fba4592d2d77dd5ee0abb5295cb8
Francis <francisd.billones@gmail.com>
2020-08-21 22:01:09 +0800

    made mario recursive instead of iterative

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -1,37 +1,32 @@
 #include <stdio.h>
 #include <cs50.h>
 
+void draw(int height);
+
 int main(void)
 {
     int size; 
     do
     {
-        size = get_int("How tall do you want your pyramid to be?:\n");
+        size = get_int("How tall do you want your pyramid to be?:");
     }
     while (size < 1 || size > 8);
     
-    //master loop for rows
-    for (int height = 0; height < size; height++)
+    draw(size);
+}
+
+void draw(int height)
+{
+    if (height == 0)
     {
-        //loop for spaces
-        for (int blank = (size - height) - 1; blank > 0; blank--)
-        {
-            printf(" ");
-        }
-        //loop for left hashes
-        for (int hashesL = (height + 1); hashesL > 0; hashesL--)
-        {
-            printf("#");
-        }
-        
-        printf("  ");
-        
-        //loop for right hashes
-        for (int spacesR = (height + 1); spacesR > 0; spacesR--)
-        {
-            printf("#");
-        }
-        
-        printf("\n");
+        return;
     }
+
+    draw(height-1);
+
+    for (int i = 0; i < height; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }
