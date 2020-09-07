b1e47943e9b82cd5c15cb26e4e6d0f40f4990130
Francis <francisd.billones@gmail.com>
2020-09-07 13:28:51 +0800

    buncha errors

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -2,8 +2,15 @@
 
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 #include "dictionary.h"
+
+#define ALPHA_FACTOR 65
+
+// Number of buckets in hash table
+const unsigned int ALPHABET = 26;
 
 // Represents a node in a hash table
 typedef struct node
@@ -13,11 +20,8 @@ typedef struct node
 }
 node;
 
-// Number of buckets in hash table
-const unsigned int N = 26;
-
 // Hash table
-node *table[N];
+node *table[ALPHABET];
 
 // var to keep track of word count
 unsigned int word_count = 0;
@@ -25,32 +29,38 @@ unsigned int word_count = 0;
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
+    node* traverse = table[hash(&word[0])]->next;
+    while (traverse != NULL)
+    {
+        if (strcasecmp(traverse->word, word) == 0)
+        {
+            return true;
+        }
+        traverse = traverse->next;
+    }
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO
-    return 0;
+    //casts char to int, converts it to uppercase, and subtracts the alpha_factor (65). 
+    //through this, the ascii value of A (65) becomes 0, B becomes 1, etc
+    return ((int) (toupper(word[0])) - ALPHA_FACTOR);
 }
 
-// places node element into the front appropriate linked list 
+// places node element into the front of appropriate linked list 
 void add_to_hash(node* hash_table[], node* node)
 {
-    //make a temporary variable for node to be placed
-    node* tmp_node = node;
-    //set node to point at same node that head is pointing to
-    node->next = hash_table[hash(node)]->next;
-    //set head to point to node
-    hash_table[hash(node)]->next = tmp_node->next;
+    //sets node's next pointer to point at the same thing head is pointing to
+    node->next = table[hash(&node->word[0])]->next;
+    //sets head to point at node
+    table[hash(&node->word[0])]->next = node;
 }
 
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    char word[];
     //open dictionary file
     FILE *dictionary_in = fopen(argv[1], "r");
 
@@ -106,6 +116,6 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    free(table);
+    return true;
 }
